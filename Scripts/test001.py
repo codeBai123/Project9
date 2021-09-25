3895745a1e5138d0b35e362f1070591b31b7d8b5
杨勇smile <1585245671@qq.com>
2021-09-25 19:12:28 +0800

    后来修改的3

--- Scripts/test001.py
+++ Scripts/test001.py
@@ -2,18 +2,16 @@ import allure
 import pytest
 
 class Test_01:
-    @pytest.allure.step(title="第一个测试")
+    @allure.step(title="第一个测试")
     @pytest.allure.severity(allure.severity_level.BLOCKER)
     def test_01(self):
         allure.attach("111", "222")
         assert 1
 
-    @pytest.allure.issue('http://www.163.com')
+    @allure.issue('http://www.163.com')
     @pytest.allure.testcase('http://www.baidu.com/test_02')
     @pytest.allure.severity(allure.severity_level.TRIVIAL)
     def test_02(self):
         allure.attach("333", "444")
         assert 1
 
-if __name__ == '__main__':
-    main(['-s', '--alluredir report', 'test001.py'])
